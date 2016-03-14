c598b6d6f6e31855ffd2cba2d81331a46005bf9e
kazz12211 <kazz12211@gmail.com>
2016-03-14 15:42:06 +0900

    Initial Commit

---
+++ KonashiI2C/KonashiI2C.h
@@ -0,0 +1,19 @@
+//
+//  KonashiI2C.h
+//  KonashiI2C
+//
+//  Created by TsubakiKazuo on H28/03/14.
+//  Copyright © 平成28年 TsubakiKazuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KonashiI2C.
+FOUNDATION_EXPORT double KonashiI2CVersionNumber;
+
+//! Project version string for KonashiI2C.
+FOUNDATION_EXPORT const unsigned char KonashiI2CVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KonashiI2C/PublicHeader.h>
+
+
